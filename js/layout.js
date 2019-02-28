22912312cb355d64f6454953348d005c4d188e6e
Sebastien AUDIER <sebastien.audier@showroomprive.net>
2019-02-28 17:28:59 +0100

    prepare start page

--- js/layout.js
+++ js/layout.js
@@ -4,13 +4,13 @@ function roundDown(number, decimals) {
 }
 
 function generateCss () {
- 	str = ' .boxes{float:left; width:100%;} .boxes.airy{margin:2%} .boxes .box{float: left; width: 100%} ';
+ 	str = ' .boxes{float:left; width:100%;} .boxes.airy{margin:2%} .boxes > .box{float: left; width: 100%} ';
 	for(var i=1; i<13; i++) {
 		margin = 2;
 		margin = roundDown(margin - Math.min((i * 0.2), 1.75), 2);
 		width = roundDown((100 /i), 2);
-		str += ' .boxes.b-' + i + ' .box {width: ' + width + '%}';
-		str += ' .boxes.b-' + i + '.airy .box {width: ' + roundDown((width - (margin * 2)),2) + '%; margin:' + margin + '%}';
+		str += ' .boxes.b-' + i + ' > .box {width: ' + width + '%}';
+		str += ' .boxes.b-' + i + '.airy > .box {width: ' + roundDown((width - (margin * 2)),2) + '%; margin:' + margin + '%}';
 		//for(var j=1; j<(10-i); j++) {
 		//	str += '.box.b-' + i + '.w-' + j +'{width: ' + roundDown((100 * (j/ (j + 1))), 2) + '%}';
 			//str += '.box.b-' + i + '.airy{width: ' + roundDown((width - (margin * 2)),2) + '%; margin:' + margin + '%}';
